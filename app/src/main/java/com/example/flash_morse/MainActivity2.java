711ad45ec21836e7d9cee5919e344a8dcb886fd7
Admin <s_vladimir902@mail.ru>
2024-03-06 13:02:53 +0600

    Add_Activity

---
+++ app/src/main/java/com/example/flash_morse/MainActivity2.java
@@ -0,0 +1,86 @@
+package com.example.flash_morse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button_1;
+
+
+    //private Button button_2;
+
+
+    private FlashClass flashClass;
+    private char[] english =  {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            ',', '.', '?'};
+    private String[] morse =  {".-", "-...", "-.-.", "-..", ".", "..-.", "–.", "....", "..",
+            ".—", "-.-", ".-..", "–", "-.", "—", ".—.", "–.-", ".-.",
+            "...", "-", "..-", "...-", ".–", "-..-", "-.–", "–..", ".—-",
+            "..—", "...–", "....-", ".....", "-....", "–...", "—..", "—-.",
+            "—–", "–..–", ".-.-.-", "..–.."};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init(){
+        button_1 = findViewById(R.id.button2);
+        flashClass = new FlashClass(this);
+    }
+
+    public void onClickFlash(View view){
+
+
+        /*if (flashClass.isFlash_status()){
+            flashClass.Off();
+        }
+        else{
+            flashClass.On();
+        }*/
+
+
+        String str = "hello";
+        char[] str_input = str.toCharArray();
+        String sum = "";
+        for(int i = 0; i < str_input.length; i++){
+            for (int j = 0; j < english.length; j++){
+                if (english[j] == str_input[i]){
+                    sum = sum + morse[j] + " ";}
+            }
+        }
+        for(int k = 0; k < sum.length(); k++) {
+            char currentChar = sum.charAt(k);
+            if (currentChar == '.') {
+                flashClass.On();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                flashClass.Off();
+            }
+            else if (currentChar == '-') {
+                flashClass.On();
+                try{
+                    Thread.sleep(1000);}
+                catch (InterruptedException e){
+                    e.printStackTrace();}
+                flashClass.Off();
+            }
+            else if (currentChar == ' ') {
+                try {
+                    Thread.sleep(1000);}
+                catch (InterruptedException e) {
+                    e.printStackTrace();}
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/flash_morse/MorseClass.java
@@ -0,0 +1,5 @@
+package com.example.flash_morse;
+
+public class MorseClass {
+
+}
