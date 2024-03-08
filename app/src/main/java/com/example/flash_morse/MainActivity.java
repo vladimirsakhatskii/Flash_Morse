e04a57e3690d6b1576ecb5110e0e932ba4fe8ab9
Admin <s_vladimir902@mail.ru>
2024-03-08 20:12:33 +0600

    First version

--- app/src/main/java/com/example/flash_morse/MainActivity.java
+++ app/src/main/java/com/example/flash_morse/MainActivity.java
@@ -5,25 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity2();
-            }
-        });
     }
-    public void openActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
+    public void onClick(View view){
+        Intent intent = new Intent(this,MainActivity2.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/flash_morse/MainActivity2.java
+++ app/src/main/java/com/example/flash_morse/MainActivity2.java
@@ -2,63 +2,37 @@ package com.example.flash_morse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
-    private Button button_1;
-    private Button button_3 = findViewById(R.id.button3);
-    private EditText editText = findViewById(R.id.editTextText2);
-
-    //private Button button_2;
-
-
+    EditText editText;
     private FlashClass flashClass;
-    private char[] english =  {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    Button button_enScript;
+    char[] english =  {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
             'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             ',', '.', '?'};
-    private String[] morse =  {".-", "-...", "-.-.", "-..", ".", "..-.", "–.", "....", "..",
-            ".—", "-.-", ".-..", "–", "-.", "—", ".—.", "–.-", ".-.",
-            "...", "-", "..-", "...-", ".–", "-..-", "-.–", "–..", ".—-",
-            "..—", "...–", "....-", ".....", "-....", "–...", "—..", "—-.",
-            "—–", "–..–", ".-.-.-", "..–.."};
+    String[] morse =  {".-", "-...", "-.-.", "-..", ".", "..-.", "-.", "....", "..",
+            ".-", "-.-", ".-..", "-", "-.", "-", ".-.", "-.-", ".-.",
+            "...", "-", "..-", "...-", ".-", "-..-", "-.-", "–..", ".--",
+            "..-", "...-", "....-", ".....", "-....", "-...", "-..", "--.",
+            "--", "-..-", ".-.-.-", "..-.."};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         init();
-        button_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openActivity3();
-            }
-        });
     }
-    public void openActivity3(){
-        Intent intent = new Intent(this, MainActivity3.class);
-        startActivity(intent);
-    }
-    private void init(){
-        button_1 = findViewById(R.id.button2);
+    public void init(){
+        editText = findViewById(R.id.editTextText);
+        button_enScript = findViewById(R.id.button2);
         flashClass = new FlashClass(this);
     }
-
-    public void onClickFlash(View view){
-
-
-        /*if (flashClass.isFlash_status()){
-            flashClass.Off();
-        }
-        else{
-            flashClass.On();
-        }*/
-
-
+    public void Enscript_Start(View view){
         String str = editText.getText().toString();
         char[] str_input = str.toCharArray();
         String sum = "";
@@ -95,5 +69,4 @@ public class MainActivity2 extends AppCompatActivity {
             }
         }
     }
-
 }
